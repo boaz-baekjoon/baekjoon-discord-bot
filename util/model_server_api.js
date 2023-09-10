54588f7242266643997e65b2c8584be43ab7bfbc
Ahn Jiwan <synoti21@naver.com>
2023-09-10 22:23:07 +0900

    :sparkles: feat: add connector with model server
    For personlized problem recommendation

--- util/model_server_api.js
+++ util/model_server_api.js
@@ -1,6 +1,31 @@
+const axios = require("axios")
+const logger = require("../logger")
 const dotenv = require("dotenv")
+const {bojProblem} = require("../models/problem");
 
 dotenv.config()
 export default class modelConnector {
+    constructor() {
+        this.baseUrl = process.env.BASE_URL;
+    }
+
+    async getRecommendedProblem(userId, problemNum){
+        let problem_arr = [];
+        const response = await axios.get(`${this.baseUrl}/api/random` , {
+            timeout: 3000,
+            params:{
+                user_id: userId,
+                num: problemNum
+            }
+        }).then(response =>{
+            problem_arr = response.data.problems;
+        }).catch(error => {
+            if (error.code === 'ECONNABORTED'){
+                logger.error(error)
+            }
+        }).finally(() => {
+            return problem_arr;
+        })
+    }
 
 }
