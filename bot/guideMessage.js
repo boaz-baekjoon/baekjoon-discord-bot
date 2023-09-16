da710e805aec0832361c378257ce8a81b937e390
Ahn Jiwan <synoti21@naver.com>
2023-09-17 00:54:01 +0900

    :recycle: refactor: rename !random -> !prob in guide message

--- bot/guideMessage.js
+++ bot/guideMessage.js
@@ -6,7 +6,7 @@ const embedWelcome = new EmbedBuilder()
     .setDescription("원하시는 서비스를 !{명령어} 형식으로 입력해주세요!")
     .addFields(
         { name: '!register', value: '백준 봇 서비스를 사용하기 위해 백준 아이디를 등록합니다.', inline: false },
-        { name: '!random', value: '백준에서 랜덤하게 문제를 추천해드립니다.', inline: false },
+        { name: '!prob', value: '개인별 맞춤 문제를 추천해드립니다.', inline: false },
         { name: '!similarid', value: '문제 번호에 해당하는 백준 문제와 유사한 문제를 추천해드립니다.', inline: false },
         { name: '!similartext', value: '문제 내용과 유사한 백준 문제를 찾아드립니다.', inline: false },
         { name: '!daily', value: '일일 맞춤형 문제를 추천 드립니다.', inline: false },
