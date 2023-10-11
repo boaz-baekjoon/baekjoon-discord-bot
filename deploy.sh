58bf467ee3821b0b486b022cfebd89c7a9e87493
Ahn Jiwan <synoti21@naver.com>
2023-10-11 21:21:03 +0900

    :green_heart: fix: write shell script properly

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,31 @@
-IS_GREEN=$(docker ps | grep boj-bot-green) # 현재 실행중인 App이 blue인지 확인
+IS_GREEN=$(docker ps | grep boj-green-container) # 현재 실행중인 App이 blue인지 확인
 
 if [ -z "$IS_GREEN"  ];then # blue라면
 
   echo "### BLUE => GREEN ###"
 
   echo "1. Get Latest Baekjoon-bot-image"
-  docker-compose pull "$DOCKERHUB_USERNAME"/"$PROJECT_NAME":latest
+  docker-compose pull boj-green-service
 
   echo "2. green container up"
-  docker-compose up -d boj-bot-green # green 컨테이너 실행
+  docker-compose up -d boj-green-service # green 컨테이너 실행
 
   sleep 5 #컨테이너 실행 5초 지연
 
   echo "3. blue container down"
-  docker-compose stop boj-bot-blue
+  docker-compose stop boj-blue-service
 
 else
   echo "### GREEN => BLUE ###"
 
   echo "1. get blue image"
-  docker-compose pull "$DOCKERHUB_USERNAME"/"$PROJECT_NAME":latest
+  docker-compose pull boj-blue-service
 
   echo "2. blue container up"
-  docker-compose up -d boj-bot-blue
+  docker-compose up -d boj-blue-service
 
   sleep 5
 
   echo "3. green container down"
-  docker-compose stop boj-bot-green
+  docker-compose stop boj-green-service
 fi
