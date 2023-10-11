8afad5169776c68ca0acde31204531fecf018721
Ahn Jiwan <synoti21@naver.com>
2023-10-12 01:19:24 +0900

    :green_heart: fix: finally succeed Blue/Green Deploy!

--- deploy.sh
+++ deploy.sh
@@ -5,27 +5,27 @@ if [ -z "$IS_GREEN"  ];then # blue라면
   echo "### BLUE => GREEN ###"
 
   echo "1. Get Latest Baekjoon-bot-image"
-  docker-compose pull boj-green-service
+  sudo docker-compose pull boj-green-service
 
   echo "2. green container up"
-  docker-compose up -d boj-green-service # green 컨테이너 실행
+  sudo docker-compose --env-file=env.list up -d boj-green-service  # green 컨테이너 실행
 
   sleep 5 #컨테이너 실행 5초 지연
 
   echo "3. blue container down"
-  docker-compose stop boj-blue-service
+  sudo docker-compose stop boj-blue-service
 
 else
   echo "### GREEN => BLUE ###"
 
   echo "1. get blue image"
-  docker-compose pull boj-blue-service
+  sudo docker-compose pull boj-blue-service
 
   echo "2. blue container up"
-  docker-compose up -d boj-blue-service
+  sudo docker-compose --env-file=env.list up -d boj-blue-service
 
   sleep 5
 
   echo "3. green container down"
-  docker-compose stop boj-green-service
+  sudo docker-compose stop boj-green-service
 fi
