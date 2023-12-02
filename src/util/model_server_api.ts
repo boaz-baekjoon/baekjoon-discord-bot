c236e1b2b181188c8319c05fe88dfc37565a4611
Ahn Jiwan <synoti21@naver.com>
2023-12-03 00:20:08 +0900

    :recycle: refactor: make new class 'ModelConnector' (#1)

--- src/util/model_server_api.ts
+++ src/util/model_server_api.ts
@@ -1,62 +1,65 @@
-const axios = require("axios")
-const logger = require("../logger")
-const dotenv = require("dotenv")
+import axios from 'axios'
+import { logger } from '../logger'
+import * as dotenv from 'dotenv'
 
 dotenv.config()
-async function getSinglePersonalizedProblems(userId, problemNum){
-    let problem_arr = [];
-    try {
-        const response = await axios.post(`${process.env.BASE_URL}/baekjun/user_id` , {
-                user_id_list: [userId],
-                problem_num: problemNum
-            },
-            {
-                timeout: 3000
-        });
-        if (Array.isArray(response.data[userId])) {
-            problem_arr = response.data[userId];
-        }
-    } catch(error) {
-        logger.error(error.message)
-    }
-    return problem_arr;
-}
 
-async function getSimilarProbWithId(probId){
-    let problem_arr = []
-    try{
-        const response = await axios.get(`${process.env.BASE_URL}/baekjun/similar_id` , {
-            timeout: 3000,
-            params:{
-                problem_id: probId,
+export class ModelConnector{
+    async getSinglePersonalizedProblems(userId, problemNum){
+        let problem_arr = [];
+        try {
+            const response = await axios.post(`${process.env.BASE_URL}/baekjun/user_id` , {
+                    user_id_list: [userId],
+                    problem_num: problemNum
+                },
+                {
+                    timeout: 3000
+                });
+            if (Array.isArray(response.data[userId])) {
+                problem_arr = response.data[userId];
             }
-        });
-        if (Array.isArray(response.data['problems'])) {
-            problem_arr = response.data['problems'];
+        } catch(error) {
+            logger.error(error.message)
         }
-    }catch(error){
-        logger.error(error.message)
+        return problem_arr;
     }
-    return problem_arr;
-}
 
-async function getSimilarProbWithContext(probContext){
-    let problem_arr= []
-    try{
-        const response = await axios.get(`${process.env.BASE_URL}/baekjun/similar_text` , {
-            timeout: 3000,
-            params:{
-                problem_text: probContext,
+    async getSimilarProbWithId(probId){
+        let problem_arr = []
+        try{
+            const response = await axios.get(`${process.env.BASE_URL}/baekjun/similar_id` , {
+                timeout: 3000,
+                params:{
+                    problem_id: probId,
+                }
+            });
+            if (Array.isArray(response.data['problems'])) {
+                problem_arr = response.data['problems'];
             }
-        });
-        if (Array.isArray(response.data['problems'])) {
-            problem_arr = response.data['problems'];
+        }catch(error){
+            logger.error(error.message)
         }
-    }catch(error){
-        logger.error(error.message)
+        return problem_arr;
     }
-    return problem_arr;
+
+    async getSimilarProbWithContext(probContext){
+        let problem_arr= []
+        try{
+            const response = await axios.get(`${process.env.BASE_URL}/baekjun/similar_text` , {
+                timeout: 3000,
+                params:{
+                    problem_text: probContext,
+                }
+            });
+            if (Array.isArray(response.data['problems'])) {
+                problem_arr = response.data['problems'];
+            }
+        }catch(error){
+            logger.error(error.message)
+        }
+        return problem_arr;
+    }
 }
 
 
-module.exports = { getSinglePersonalizedProblems, getSimilarProbWithId, getSimilarProbWithContext }
+
