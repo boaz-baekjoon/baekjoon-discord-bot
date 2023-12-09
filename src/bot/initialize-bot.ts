1c1d89d2632d682b3436a61d723485a3c6f22cfc
Ahn Jiwan <synoti21@naver.com>
2023-12-09 19:23:22 +0900

    :recycle: refactor: successfully initailized bot (#1)

--- src/bot/initialize-bot.ts
+++ src/bot/initialize-bot.ts
@@ -4,25 +4,29 @@ import * as fs from "fs";
 
 export async function initializeBot(client: Client){
     dotenv.config();
+    try{
+        //Set default collections for commands
+        client.commands = new Collection();
 
-    //Set default collections for commands
-    client.commands = new Collection();
+        //Login Discord bot with Discord_Token
+        await client.login(process.env.DISCORD_TOKEN);
+        console.log("Discord Bot Logged In")
 
-    //Login Discord bot with Discord_Token
-    await client.login(process.env.DISCORD_TOKEN);
+        //Loading Commands
+        const commands = fs.readdirSync("./src/commands").filter(file => file.endsWith(".ts"));
+        for (const file of commands) {
+            const commandName = file.split(".")[0];
+            const command = require(`../commands/${file}`);
 
-    //Loading Commands
-    const commands = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
-    for (const file of commands) {
-        const commandName = file.split(".")[0];
-        const command = require(`./commands/${file}`);
+            console.log(`Attempting to load command ${commandName}`);
+            client.commands.set(commandName, command);
+        }
 
-        console.log(`Attempting to load command ${commandName}`);
-        client.commands.set(commandName, command);
+        //Initialize Success
+        client.once('ready', async () => {
+            console.log("BOJ Bot is ready")
+        })
+    }catch (error){
+        console.error(error)
     }
-
-    //Initialize Success
-    client.once('ready', async () => {
-        console.log("BOJ Bot is ready")
-    })
 }
