3c8a2a5bbd7597b0ffa7e0c1e09e1ddccdd34e9d
Ahn Jiwan <synoti21@naver.com>
2024-01-04 17:45:32 +0900

    :sparkles: feat: activate transcation, export help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,6 +1,6 @@
 import {embedWelcome} from '../bot/guideMessage.js';
 import {Message} from "discord.js";
 
-async function execute(message: Message){
+export async function execute(message: Message){
     message.channel.send({embeds: [embedWelcome]})
 }
