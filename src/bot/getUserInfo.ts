4c7f1ef0b2eb236e417b686f63cccd033c201c1c
Ahn Jiwan <synoti21@naver.com>
2024-01-12 01:42:32 +0900

    :sparkles: feat: get user info via solved ac (#23)

---
+++ src/bot/getUserInfo.ts
@@ -0,0 +1,20 @@
+import {SolvedAcUser} from "../model/solvedac_user_class.js";
+import axios from "axios";
+import {logger} from "../logger.js";
+
+export async function searchUserInfoWithSolvedAc(userId: string): Promise<SolvedAcUser> {
+    try {
+        const response = await axios.get(`https://solved.ac/api/v3/user/show`, {
+            timeout: 3000,
+            params: {
+                handle: userId,
+            }
+        });
+        if (response.data['handle'] === userId) {
+            return SolvedAcUser.returnUserWithResponse(response.data);
+        }
+    } catch (error: any) {
+        logger.error(error.message)
+    }
+    return SolvedAcUser.returnErrorInstance();
+}
