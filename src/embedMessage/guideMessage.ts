552938608b681a893798993867c1bcaa72a41a7c
Ahn Jiwan <synoti21@naver.com>
2024-01-27 15:24:05 +0900

    :memo: docs: update help message

--- src/embedMessage/guideMessage.ts
+++ src/embedMessage/guideMessage.ts
@@ -10,7 +10,7 @@ export const embedWelcome = new EmbedBuilder()
         { name: '!register', value: '백준 봇 서비스를 사용하기 위해 백준 아이디를 등록합니다.', inline: false },
         { name: '!prob', value: '개인별 맞춤 문제를 추천해드립니다.', inline: false },
         { name: '!similarid', value: '문제 번호에 해당하는 백준 문제와 유사한 문제를 추천해드립니다.', inline: false },
-        { name: '!daily', value: '일일 맞춤형 문제를 추천 드립니다.', inline: false },
+        { name: '!daily', value: '일일 맞춤형 문제를 매일 정해진 시간에 추천 드립니다.', inline: false },
         { name: '!category', value: '알고리즘 분류 목록을 보여드립니다.', inline: false },
         { name: '!help', value: '백준 봇 명령어를 알려드립니다.', inline: false },
         { name: '\u200B', value: '\u200B' }
